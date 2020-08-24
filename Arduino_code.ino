75c5139f97dc66f4307cdf2bc41a30897108b4e8
Vedant <32189159+Vedantdesai2511@users.noreply.github.com>
2020-08-24 11:53:51 -0400

    Updated the loop function added functionalities
    Added functionalities, initiated callbacks for the robot to work.

--- Arduino_code.ino
+++ Arduino_code.ino
@@ -31,5 +31,116 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  void loop() {
+  // Read the incoming data from the Smartphone Android App
+  while (Bluetooth.available() >= 2) {
+    xDir = Bluetooth.read();
+    delay(10);
+    yDir = Bluetooth.read();
+    Serial.print(xDir);
+    Serial.print(",");
+    Serial.println(yDir);
+  }
+  delay(10);
+  // Makes sure we receive correct values
+  //Custom Inputs - Straight, Circle, Sine, Stop, Rotate In Place
+  if (xDir == 5)
+  {
+    switch (yDir)
+    {
+      case 172: // Go straight
+        motorSpeed1 = 250;
+        motorSpeed2 = 250;
+        forward();
+        break;
+      case 174: // Circle
+        motorSpeed1 = 250;
+        motorSpeed2 = 200;
+        forward();
+        break;
+      case 176: // Sine or Wave motion
+        if (toggleFlag)
+        {
+          motorSpeed1 = 250;
+          motorSpeed2 = 200;
+        }
+        else
+        {
+          motorSpeed1 = 200;
+          motorSpeed2 = 250;
+        }
+        curve();
+        break;
+      case 178: //Rotate in place
+        motorSpeed1 = 250;
+        motorSpeed2 = 250;
+        turnRight();
+        break;
+      case 180: //Stop
+        Stop();
+        break;
+    }
+  }
+  else
+  {
+    //Joystick Control
+    if (xDir > 50 && xDir < 120 && yDir > 50 && yDir < 120) {
+      Stop();
+    }
+    if (yDir > 50 && yDir < 120) {
+      if (xDir < 50) {
+        turnRight();
+        motorSpeed1 = map(xDir, 50, 10, 0, 255);
+        motorSpeed2 = map(xDir, 50, 10, 0, 255);
+      }
+      if (xDir > 120) {
+        turnLeft();
+        motorSpeed1 = map(xDir, 120, 150, 0, 255);
+        motorSpeed2 = map(xDir, 120, 150, 0, 255);
+      }
+    } 
+    else {
+      if (xDir > 50 && xDir < 120) {
+
+        if (yDir < 50) {
+          forward();
+        }
+        if (yDir > 120) {
+          backward();
+        }
+        if (yDir < 50) {
+          motorSpeed1 = map(yDir, 50, 10, 0, 255);
+          motorSpeed2 = map(yDir, 50, 10, 0, 255);
+        }
+        if (yDir > 120) {
+          motorSpeed1 = map(yDir, 120, 150, 0, 255);
+          motorSpeed2 = map(yDir, 120, 150, 0, 255);
+        }
+      } 
+      else {
+        if (yDir < 50) {
+          forward();
+        }
+        if (yDir > 120) {
+          backward();
+        }
+        if (xDir < 50) {
+          motorSpeed1 = map(xDir, 50, 10, 255, 50);
+          motorSpeed2 = 255;
+        }
+        if (xDir > 120) {
+          motorSpeed1 = 255;
+          motorSpeed2 = map(xDir, 120, 150, 255, 50);
+        }
+      }
+    }
+  }
+  //Serial.print(motorSpeed1);
+  //Serial.print(",");
+  //Serial.println(motorSpeed1);
+
+  analogWrite(en1, motorSpeed1); // Send PWM signal to motor A
+  analogWrite(en2, motorSpeed2); // Send PWM signal to motor B
+}
 
 }
